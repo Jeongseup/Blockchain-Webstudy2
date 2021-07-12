cf644fed6609421e738b44581ed154dc534eb167
Jeongseup <thswjdtmq4@inu.ac.kr>
2021-07-12 22:10:13 +0900

    [Day32] get DB data by node.js

---
+++ src/views/Person.vue
@@ -0,0 +1,110 @@
+<template>
+    <div>
+        <div>
+            <label>first name : </label>
+            <input type="text" v-model="firstName" />
+
+            <label>last name : </label>
+            <input type="text" v-model="lastName" />
+
+            <label>email : </label>
+            <input type="text" v-model="email" />
+        </div>
+        <button @click="createPerson" @keyup.enter="createPerson">
+            생성
+        </button>
+        <div>
+            <table style="width:100%;">
+                <thead>
+                    <tr>
+                        <th>fisrt name</th>
+                        <th>last name</th>
+                        <th>email</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr :key="person.id" v-for="(person, idx) in list">
+                        <td>
+                            <input type="text" v-model="person.first_name" />
+                        </td>
+                        <td>
+                            <input type="text" v-model="person.last_name" />
+                        </td>
+                        <td><input type="text" v-model="person.email" /></td>
+                        <td>
+                            <button @click="deletePerson(person.id)">
+                                삭제
+                            </button>
+                        </td>
+                        <td>
+                            <button @click="updatePerson(idx)">수정</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: '',
+    components: {},
+    data() {
+        return {
+            list: [],
+            firstName: '',
+            lastName: '',
+            email: ''
+        }
+    },
+    setup() {},
+    created() {},
+    mounted() {
+        this.getList()
+    },
+    unmounted() {},
+    methods: {
+        async getList() {
+            this.list = await this.$api('/api/getList', 'get', {})
+            console.log(this.list)
+        },
+
+        async createPerson() {
+            const r = await this.$api('/api/createPerson', 'post', {
+                param: [
+                    {
+                        first_name: this.firstName,
+                        last_name: this.lastName,
+                        email: this.email
+                    }
+                ]
+            })
+            this.getList()
+            console.log('r is', r)
+        },
+        async deletePerson(id) {
+            const r = await this.$api('/api/deletePerson', 'post', {
+                param: [id]
+            })
+            this.getList()
+            console.log('r is', r)
+        },
+        async updatePerson(idx) {
+            const r = await this.$api('/api/updatePerson', 'post', {
+                param: [
+                    {
+                        first_name: this.list[idx].first_name,
+                        last_name: this.list[idx].last_name,
+                        email: this.list[idx].email
+                    },
+                    this.list[idx].id
+                ]
+            })
+            this.getList()
+            console.log(r)
+        }
+    }
+}
+</script>
